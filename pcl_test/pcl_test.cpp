df4cfdb85f94588d71e0cccaf2b75c2788177b45
KazuyukiArimatsu <mohammedarixx@gmail.com>
2014-02-07 16:35:01 +0900

    first commit.

---
+++ pcl_test/pcl_test.cpp
@@ -0,0 +1,30 @@
+// pcl_test.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "stdafx.h"
+
+#include <pcl/io/pxc_grabber.h>
+#include <pcl/visualization/cloud_viewer.h>
+
+void grabberCallback(pcl::visualization::CloudViewer &viewer, const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr &cloud)
+{
+    if (!viewer.wasStopped())
+        viewer.showCloud(cloud);
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    pcl::visualization::CloudViewer viewer("Senz3D Viewer");
+    pcl::PXCGrabber grabber;
+
+    boost::function<void (const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr&)> f = boost::bind(&grabberCallback, boost::ref(viewer), _1);
+    grabber.registerCallback(f);
+    grabber.start();
+
+    while(!viewer.wasStopped())
+        Sleep(100);
+
+    grabber.stop();
+	return 0;
+}
+
